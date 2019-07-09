24ab28c8f3061dc923bce516b6de0914dd5571e4
matanhs <matan.hrs@gmail.com>
2019-07-09 19:15:00 +0300

    partial class fixup

--- partial_class.py
+++ partial_class.py
@@ -3,5 +3,6 @@ __all__ = ['partial_class']
 
 def partial_class(cls, *args, **kwargs):
     class PartialClass(cls):
+        source_class = cls
         __init__ = partialmethod(cls.__init__, *args, **kwargs)
     return PartialClass

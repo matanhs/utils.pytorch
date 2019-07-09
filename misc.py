ff7768ee7f63015907704730b08b5e3572d872db
matanhs <matan.hrs@gmail.com>
2019-07-09 19:21:43 +0300

    smoothing layer;channel wise cosine similarity

--- misc.py
+++ misc.py
@@ -64,3 +64,111 @@ class CheckpointModule(nn.Module):
             return checkpoint_sequential(self.module, self.num_segments, x)
         else:
             return checkpoint(self.module, x)
+
+class View(nn.Module):
+    def __init__(self, *shape):
+        super(View, self).__init__()
+        self.shape = shape
+    def forward(self, input):
+        return input.view(*self.shape)
+
+
+import math
+import numbers
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class CosineSimilarityChannelWiseLoss(nn.Module):
+    def __init__(self,reduction='mean'):
+        assert reduction in ['mean','sum','max']
+        super(CosineSimilarityChannelWiseLoss,self).__init__()
+        self.reduction = reduction
+
+    def forward(self, input,target):
+        #per_channel_loss = torch.zeros((input.size(1)),device=input.device)
+        # channel first
+
+        per_channel_loss = F.cosine_similarity(input.transpose(1,0).view(input.size(1),input.size(0),-1),target.transpose(1,0).view(target.size(1),target.size(0),-1),-1).mean(1)
+        # channel reduction:
+        if self.reduction == 'mean':
+            return per_channel_loss.mean()
+        elif self.reduction == 'sum':
+            return per_channel_loss.sum()
+        elif self.reduction == 'max':
+            return per_channel_loss.max()
+
+
+class GaussianSmoothing(nn.Module):
+    # code from https://discuss.pytorch.org/t/is-there-anyway-to-do-gaussian-filtering-for-an-image-2d-3d-in-pytorch/12351/9
+    """
+    Apply gaussian smoothing on a
+    1d, 2d or 3d tensor. Filtering is performed seperately for each channel
+    in the input using a depthwise convolution.
+    Arguments:
+        channels (int, sequence): Number of channels of the input tensors. Output will
+            have this number of channels as well.
+        kernel_size (int, sequence): Size of the gaussian kernel.
+        sigma (float, sequence): Standard deviation of the gaussian kernel.
+        dim (int, optional): The number of dimensions of the data.
+            Default value is 2 (spatial).
+    """
+    def __init__(self, channels, kernel_size, sigma, dim=2):
+        super(GaussianSmoothing, self).__init__()
+        if isinstance(kernel_size, numbers.Number):
+            kernel_size = [kernel_size] * dim
+        if isinstance(sigma, numbers.Number):
+            sigma = [sigma] * dim
+        if isinstance(sigma, torch.Tensor):
+            sigma = [sigma.item()] * dim
+
+        # The gaussian kernel is the product of the
+        # gaussian function of each dimension.
+        kernel = 1
+        meshgrids = torch.meshgrid(
+            [
+                torch.arange(size, dtype=torch.float32)
+                for size in kernel_size
+            ]
+        )
+        for size, std, mgrid in zip(kernel_size, sigma, meshgrids):
+            mean = (size - 1) / 2
+            kernel *= 1 / (std * math.sqrt(2 * math.pi)) * \
+                      torch.exp(-((mgrid - mean) / std) ** 2 / 2)
+
+        # Make sure sum of values in gaussian kernel equals 1.
+        kernel = kernel / torch.sum(kernel)
+
+        # Reshape to depthwise convolutional weight
+        kernel = kernel.view(1, 1, *kernel.size())
+        kernel = kernel.repeat(channels, *[1] * (kernel.dim() - 1))
+
+        self.register_buffer('weight', kernel)
+        self.groups = channels
+        self.pad = kernel_size[0] // 2
+        if dim == 1:
+            self.conv = F.conv1d
+        elif dim == 2:
+            self.conv = F.conv2d
+        elif dim == 3:
+            self.conv = F.conv3d
+        else:
+            raise RuntimeError(
+                'Only 1, 2 and 3 dimensions are supported. Received {}.'.format(dim)
+            )
+
+    def forward(self, input):
+        """
+        Apply gaussian filter to input.
+        Arguments:
+            input (torch.Tensor): Input to apply gaussian filter on.
+        Returns:
+            filtered (torch.Tensor): Filtered output.
+        """
+        return self.conv(input, weight=self.weight, groups=self.groups,padding = self.pad)
+
+
+# smoothing = GaussianSmoothing(3, 5, 1)
+# input = torch.rand(1, 3, 100, 100)
+# input = F.pad(input, (2, 2, 2, 2), mode='reflect')
+# output = smoothing(input)

c48d8c201eabab7f1fdfa1dbf3ba4f387f44672c
matanhs <matan.hrs@gmail.com>
2020-02-18 20:15:45 +0200

    recorder class for activations and gradients; todo test and polish

--- misc.py
+++ misc.py
@@ -2,8 +2,12 @@ import random
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from torch.utils.checkpoint import checkpoint, checkpoint_sequential
 from argparse import ArgumentParser
+import math
+import numbers
+from _collections import OrderedDict
 torch_dtypes = {
     'float': torch.float,
     'float32': torch.float32,
@@ -124,11 +128,6 @@ def get_lambda_module_class(fn,name=None):
 
     return LambdaModule
 
-import math
-import numbers
-import torch
-from torch import nn
-from torch.nn import functional as F
 
 class CosineSimilarityChannelWiseLoss(nn.Module):
     def __init__(self,reduction='mean'):
@@ -223,3 +222,63 @@ class GaussianSmoothing(nn.Module):
 # input = torch.rand(1, 3, 100, 100)
 # input = F.pad(input, (2, 2, 2, 2), mode='reflect')
 # output = smoothing(input)
+class Recorder():
+    _RECORDING_MODES=['inputs','output']
+    _DEVICE_MODES=['same','cpu']
+
+    def __init__(self,model,recording_mode=['inputs'],
+                 exclude_matcher_fn=lambda n,m: False,
+                 include_matcher_fn=lambda n,m: True,
+                 input_fn=None, output_fn=None,
+                 grad_in_fn=None, grad_out_fn=None,
+                 activation_reducer_fn=None,grad_reducer_fn=None,
+                 include_gradients=False,
+                 device_mode='cpu'):
+        Recorder._assert_supported_static(recording_mode,'_RECORDING_MODES')
+        Recorder._assert_supported_static(device_mode,'_DEVICE_MODES')
+        Recorder._assert_callable_or_none([input_fn,output_fn,grad_in_fn,grad_out_fn,
+                                           activation_reducer_fn,grad_reducer_fn])
+        self.record = OrderedDict()
+        self.recording_mode = recording_mode
+        self.device_modifier = lambda v: v if device_mode == 'same' else v.detach().cpu()
+        for trace_name,m in model.named_children():
+            if include_matcher_fn(trace_name,m) and not exclude_matcher_fn(trace_name,m):
+                m.register_forward_hook(self.recording_hook_generator(trace_name,input_fn,output_fn,
+                                                                      activation_reducer_fn))
+                if include_gradients:
+                    m.register_backward_hook(self.recording_hook_generator(trace_name,grad_in_fn,grad_out_fn,grad_reducer_fn))
+
+    @staticmethod
+    def _assert_supported_static(i,static_attr):
+        if hasattr(i,'__iter__'):
+            assert all([a in getattr(Recorder,static_attr) for a in i])
+        else:
+            assert i in getattr(Recorder,static_attr)
+
+    @staticmethod
+    def _assert_callable_or_none(candidates):
+        assert all([callable(fn) or fn is None for fn in candidates])
+
+    def stack_new_entries(self,k,v,reducer_fn=None):
+            if k in self.record:
+                if reducer_fn:
+                    self.record[k]=(self.record[k],v)
+                else:
+                    self.record[k] = torch.cat([self.record[k],self.device_modifier(v)])
+            else:
+                self.record[k] = self.device_modifier(v)
+
+    def recording_hook_generator(self,trace_name,in_fn=None,out_fn=None,reducer_fn=None):
+        def recorder_hook(m, inputs, output):
+            if 'inputs' in self.recording_mode:
+                for i,inp in enumerate(inputs):
+                    self.stack_new_entries(trace_name + '_input:{i}',inp,reducer_fn)
+                if in_fn:
+                    in_fn(inputs)
+                    self.stack_new_entries(trace_name+'_input_fn',reducer_fn)
+            if 'output' in self.recording_mode:
+                self.stack_new_entries(trace_name + '_output',output,reducer_fn)
+                if out_fn:
+                    out_fn(inputs)
+                    self.stack_new_entries(trace_name+'_output_fn',reducer_fn)
+            return recorder_hook
